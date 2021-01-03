8cacc5ea6255bd8d930483b65f0a5d1b349cf8e2
Dang <dang@travelstop.com>
2021-01-03 19:53:19 +0800

    faster

--- conf.d/travelstop-prompt.fish
+++ conf.d/travelstop-prompt.fish
@@ -1,19 +1,31 @@
 status is-interactive || exit
 
-function __travelstop-prompt-enable
+set --local STARSHIP_CONFIG_ORIGINAL $STARSHIP_CONFIG
+set --global --export travelstop_prompt_aws_profile (string replace --regex '@.*' '' "$AWS_PROFILE")
+set --global --export travelstop_prompt_aws_stage (string replace --regex '.*@' '' "$AWS_PROFILE")
+
+function __travelstop_prompt_toggle --on-variable PWD
   set --query travelstop_dir || set --global travelstop_dir '/wip/travelstop/'
-  string match --quiet "*$travelstop_dir*" (pwd)/
+  if string match --quiet "*$travelstop_dir*" "$PWD/"
+    set --global --export STARSHIP_CONFIG ~/.config/fish/conf.d/travelstop-prompt.toml
+    set --global travelstop_prompt_enabled
+  else
+    set --global --export STARSHIP_CONFIG $STARSHIP_CONFIG_ORIGINAL
+    set --erase --global travelstop_prompt_enabled
+  end
 end
 
-function __travelstop-prompt-repaint --on-variable AWS_PROFILE
+__travelstop_prompt_toggle
+
+function __travelstop_prompt_repaint --on-variable AWS_PROFILE
+  set --global --export travelstop_prompt_aws_profile (string replace --regex '@.*' '' "$AWS_PROFILE")
+  set --global --export travelstop_prompt_aws_stage (string replace --regex '.*@' '' "$AWS_PROFILE")
   commandline --function repaint
 end
 
 function __restore_last_status --argument-names last_status
   return $last_status
 end
-
-set --local STARSHIP_CONFIG_ORIGINAL $STARSHIP_CONFIG
 
 functions --query fish_prompt_original || functions --copy fish_prompt fish_prompt_original
 starship init fish | source
@@ -22,13 +34,11 @@ functions --copy fish_prompt fish_prompt_travelstop
 
 function fish_prompt
   set last_status $status
-  if __travelstop-prompt-enable
+  if set --query --global travelstop_prompt_enabled
     __restore_last_status $last_status
-    set --export STARSHIP_CONFIG ~/.config/fish/conf.d/travelstop-prompt.toml
     fish_prompt_travelstop
   else
     __restore_last_status $last_status
-    set --export STARSHIP_CONFIG $STARSHIP_CONFIG_ORIGINAL
     fish_prompt_original
   end
 end
